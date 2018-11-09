ee4b2ec500bac89f15260ac86b020f9d7487f2e1
ShivamShrirao <shivamshrirao@gmail.com>
2018-11-10 04:16:34 +0530

    pass

--- digit_classifier.py
+++ digit_classifier.py
@@ -2,6 +2,7 @@
 
 import nnet
 import pickle
+import sys
 import numpy as np
 from mnist import MNIST
 from time import time
@@ -9,19 +10,20 @@ from time import time
 print("Loading dataset....")
 mndata = MNIST('../mnist_dataset')
 X_train, y_train = mndata.load_training()
-X_train = mndata.process_images_to_numpy(X_train)/255
+X_train = (mndata.process_images_to_numpy(X_train)/255)
 X_test, y_test = mndata.load_testing()
-X_test = mndata.process_images_to_numpy(X_test)/255
+X_test = (mndata.process_images_to_numpy(X_test)/255)
 
-cnn=nnet.neural_net(nrons=[784,150,40,10])		# 784,100,50,10
+cnn=nnet.neural_net(nrons=[784,150,40,10])
 cnn.learning_rate=0.1
 cnn.activations(func=['sigmoid','sigmoid','sigmoid'])
+# cnn.activations(func=['relu','relu','softmax'])
 y_inp=np.zeros((len(y_train),10))
 for i in range(len(y_train)):
 	y_inp[i][y_train[i]]=1
 
 print("Training....")
-epoch=epochs=1
+epoch=epochs=10
 
 total_t=time()
 while epoch>0:
@@ -34,6 +36,7 @@ while epoch>0:
 		ans=out.argmax()
 		cnn.backprop(y_inp[i])
 		cor=y_train[i]
+		# sys.exit(0)
 		if ans == cor:
 			correct+=1
 		if not i%1000:
@@ -43,10 +46,11 @@ while epoch>0:
 		cnn.learning_rate=0.01
 	print("Accuracy:",(correct*100/len(y_train)),'%')
 	print("Time:",(time()-t))
+# sys.exit(0)
 
 print("\nTotal time:",(time()-total_t),"sec")
-# with open('trained.dump','wb') as f:
-# 	pickle.dump(cnn,f)
+with open('trained.dump','wb') as f:
+	pickle.dump(cnn,f)
 
 print("Calculating accuracy....")
 
--- nnet.py
+++ nnet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 
-sd=470#np.random.randint(500)
+sd=470#np.random.randint(1000)
 print(sd)
 np.random.seed(sd)	#470
 
@@ -11,8 +11,9 @@ class neural_net:
 		self.weights=[]
 		self.bias=[]
 		self.learning_rate=0.1
+		self.cross=False
 		for i in range(len(self.nrons)-1):
-			self.weights.append(2*np.random.rand(self.nrons[i],self.nrons[i+1])-1)
+			self.weights.append(np.random.randn(self.nrons[i],self.nrons[i+1])*np.sqrt(2/self.nrons[i]))
 			self.bias.append(2*np.random.rand(1,self.nrons[i+1])-1)
 
 	def __str__(self):
@@ -31,15 +32,24 @@ class neural_net:
 		return (y > 0)*1
 
 	def softmax(self,x):
-		exps = np.exp(x)
-		# exps = np.exp(x-np.max(x))
+		# exps = np.exp(x)
+		exps = np.exp(x-np.max(x))
 		return exps/np.sum(exps)
 
 	def soft_der(self,x,y):
-		return x * (1 - x)
+		# return -x*y
+		return 1
+
+	def del_cross_soft(self,out,res):
+		res = res.argmax(axis=1)
+		m = res.shape[0]
+		grad = out
+		grad[range(m),res] -= 1
+		grad = grad/m
+		return grad
 
 	def activations(self,func):
-		self.func=func							# ['sigmoid','relu','softmax']
+		self.func=func							# ['relu','sigmoid','softmax']
 		self.activate=[]
 		self.act_der=[]
 		for i in range(len(func)):
@@ -52,6 +62,8 @@ class neural_net:
 			elif func[i]=='sigmoid':
 				self.activate.append(self.sigmoid)
 				self.act_der.append(self.sigmoid_der)
+		if func[-1]=='softmax':
+			self.cross=True
 
 	def feed_forward(self, X):
 		self.X = X.reshape(1,self.nrons[0])
@@ -60,13 +72,18 @@ class neural_net:
 		for i in range(len(self.nrons)-1):
 			self.z.append(np.dot(self.a[i] ,self.weights[i])+self.bias[i])	# w0(784,20) w1(20,20) w2(20,10)
 			self.a.append(self.activate[i](self.z[-1]))		# a1(1,20) a2(1,20) b
+		# print(self.z[2])
+		# print(self.a[3])
 		return self.a[-1][0]					# a3(1,10)
 
 	def backprop(self, y):
-		self.y = y 								# (1,10)
-		d_c_a = 2*(self.y-self.a[-1])
+		self.y = y.reshape(1,self.nrons[-1]) 				# (1,10)
+		if self.cross:
+			d_c_a = self.del_cross_soft(self.a[-1],self.y)
+		else:
+			d_c_a = 2*(self.y-self.a[-1])
 		for i in range((len(self.nrons)-2), -1, -1):
-			d_c_b = d_c_a*self.act_der[i](self.a[i+1],self.z[i])
+			d_c_b = d_c_a*(self.act_der[i](self.a[i+1],self.z[i]))
 			d_c_w = np.dot(self.a[i].T, d_c_b)
 			d_c_a = np.dot(d_c_b, self.weights[i].T)
 			self.weights[i]+=(d_c_w*self.learning_rate)
